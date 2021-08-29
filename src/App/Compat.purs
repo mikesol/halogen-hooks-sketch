3666ca396cd1873dcc38c962c11b34f49349df29
Mike Solomon <mike@meeshkan.com>
2021-08-29 08:27:20 +0300

    Adds useLifecycleEffect

--- src/App/Compat.purs
+++ src/App/Compat.purs
@@ -1,17 +1,34 @@
-module App.Hooks.Compat where
+module App.Hooks.Compat
+  ( useState
+  , useQuery
+  , useTickEffect
+  , useLifecycleEffect
+  , component
+  , modify_
+  , capture
+  , QueryToken
+  , SlotsToken
+  , OutputToken
+  , Q
+  , F
+  , pure
+  , bind
+  , discard
+  ) where
 
 import Prelude
-import App.Hooks (getHookCons)
 import App.Hooks as Hooks
 import App.Sugar as Sugar
-import Control.Applicative.Indexed (class IxApplicative, ipure)
+import Control.Applicative.Indexed (class IxApplicative, ipure, (:*>))
 import Control.Bind.Indexed (class IxBind, class IxDiscard, ibind, idiscard, imap)
 import Control.Monad.Indexed.Qualified as Ix
-import Data.Maybe (Maybe(..))
+import Data.Maybe (Maybe(..), fromMaybe)
 import Data.Symbol (class IsSymbol)
+import Data.Traversable (sequence)
 import Data.Tuple.Nested (type (/\), (/\))
 import Halogen as H
 import Prelude as Applicative
+import Prelude as Bind
 import Prim.Row as Row
 import Prim.RowList as RL
 import Prim.Symbol as Symbol
@@ -39,6 +56,7 @@ data Q query hooks' input slots output m
       query a ->
       Hooks.HookM
         ( "" :: Q query hooks' input slots output m
+        , "_" :: F query hooks' input slots output m
         | hooks'
         )
         input
@@ -55,6 +73,7 @@ unQ ::
     query a ->
     Hooks.HookM
       ( "" :: Q query hooks' input slots output m
+      , "_" :: F query hooks' input slots output m
       | hooks'
       )
       input
@@ -65,13 +84,48 @@ unQ ::
   )
 unQ (Q q) = q
 
+data F query hooks' input slots output m
+  = F
+    ( Array
+        ( Hooks.HookM
+            ( "" :: Q query hooks' input slots output m
+            , "_" :: F query hooks' input slots output m
+            | hooks'
+            )
+            input
+            slots
+            output
+            m
+            Unit
+        )
+    )
+
+unF ::
+  forall query hooks' input slots output m.
+  F query hooks' input slots output m ->
+  Array
+    ( Hooks.HookM
+        ( "" :: Q query hooks' input slots output m
+        , "_" :: F query hooks' input slots output m
+        | hooks'
+        )
+        input
+        slots
+        output
+        m
+        Unit
+    )
+unF (F q) = q
+
 component ::
   forall (query :: Type -> Type) (hooks' :: Row Type) (input :: Type) (slots :: Row Type) (output :: Type) (m :: Type -> Type).
   Row.Lacks "" hooks' =>
+  Row.Lacks "_" hooks' =>
   ( ComponentTokens query slots output ->
     input ->
     Hooks.IndexedHookM
       ( "" :: Q query hooks' input slots output m
+      , "_" :: F query hooks' input slots output m
       | hooks'
       )
       input
@@ -79,12 +133,15 @@ component ::
       output
       m
       ( "" :: Q query hooks' input slots output m
+      , "_" :: F query hooks' input slots output m
       )
       ( "" :: Q query hooks' input slots output m
+      , "_" :: F query hooks' input slots output m
       | hooks'
       )
       ( Hooks.HookHTML
           ( "" :: Q query hooks' input slots output m
+          , "_" :: F query hooks' input slots output m
           | hooks'
           )
           input
@@ -100,16 +157,23 @@ component f =
         { handleQuery =
           \q ->
             Hooks.getHooksM
-              >>= \hooks -> case getHookCons (Proxy :: _ "") hooks of
+              >>= \hooks -> case Hooks.getHookCons (Proxy :: _ "") hooks of
                   Nothing -> Applicative.pure Nothing
                   Just fun -> (unQ fun) q
+        , finalize =
+          Hooks.getHooksM
+            >>= \hooks -> case Hooks.getHookCons (Proxy :: _ "_") hooks of
+                Nothing -> Applicative.pure unit
+                Just arr -> void $ sequence (unF arr)
         }
     )
     go
   where
   start =
     Hooks.hookCons (Proxy :: _ "")
-      (Applicative.pure ((Q (const $ Applicative.pure Nothing))))
+      (Applicative.pure (Q (const $ Applicative.pure Nothing)))
+      :*> Hooks.hookCons (Proxy :: _ "_")
+          (Applicative.pure (F []))
 
   go =
     ( \i -> Ix.do
@@ -122,6 +186,113 @@ component f =
           i
     )
 
+useLifecycleEffect ::
+  forall i o hooks'' input slots output m query hooks' iRL sym' sym.
+  IsSymbol sym =>
+  RL.RowToList i iRL =>
+  Sugar.GetLexicalLast "" iRL sym' =>
+  Symbol.Append sym' "_" sym =>
+  Row.Lacks sym i =>
+  Row.Cons sym Unit i o =>
+  Row.Lacks sym hooks'' =>
+  Row.Cons
+    sym
+    Unit
+    hooks''
+    ( "" :: Q query hooks' input slots output m
+    , "_" :: F query hooks' input slots output m
+    | hooks'
+    ) =>
+  Hooks.HookM
+    ( "" :: Q query hooks' input slots output m
+    , "_" :: F query hooks' input slots output m
+    | hooks'
+    )
+    input
+    slots
+    output
+    m
+    ( Maybe
+        ( Hooks.HookM
+            ( "" :: Q query hooks' input slots output m
+            , "_" :: F query hooks' input slots output m
+            | hooks'
+            )
+            input
+            slots
+            output
+            m
+            Unit
+        )
+    ) ->
+  Hooks.IndexedHookM
+    ( "" :: Q query hooks' input slots output m
+    , "_" :: F query hooks' input slots output m
+    | hooks'
+    )
+    input
+    slots
+    output
+    m
+    i
+    o
+    Unit
+useLifecycleEffect m = Hooks.hookCons (Proxy :: _ sym) (Applicative.pure unit) :*> useTickEffect m
+
+useTickEffect ::
+  forall query hooks' input slots output m i.
+  Hooks.HookM
+    ( "" :: Q query hooks' input slots output m
+    , "_" :: F query hooks' input slots output m
+    | hooks'
+    )
+    input
+    slots
+    output
+    m
+    ( Maybe
+        ( Hooks.HookM
+            ( "" :: Q query hooks' input slots output m
+            , "_" :: F query hooks' input slots output m
+            | hooks'
+            )
+            input
+            slots
+            output
+            m
+            Unit
+        )
+    ) ->
+  Hooks.IndexedHookM
+    ( "" :: Q query hooks' input slots output m
+    , "_" :: F query hooks' input slots output m
+    | hooks'
+    )
+    input
+    slots
+    output
+    m
+    i
+    i
+    Unit
+useTickEffect i =
+  Hooks.lift
+    ( Bind.bind i \iRes ->
+        Bind.bind
+          ( map
+              ( fromMaybe (F [])
+                  <<< Hooks.getHookCons (Proxy :: _ "_")
+              )
+              Hooks.getHooksM
+          ) \(F arr) ->
+          void
+            $ Hooks.setHookMCons (Proxy :: _ "_")
+            $ F
+            $ case iRes of
+                Nothing -> arr
+                Just iRes' -> [ iRes' ] <> arr
+    )
+
 useQuery ::
   forall query hooks' input slots output m i.
   QueryToken query ->
@@ -129,6 +300,7 @@ useQuery ::
     query a ->
     Hooks.HookM
       ( "" :: Q query hooks' input slots output m
+      , "_" :: F query hooks' input slots output m
       | hooks'
       )
       input
@@ -139,6 +311,7 @@ useQuery ::
   ) ->
   Hooks.IndexedHookM
     ( "" :: Q query hooks' input slots output m
+    , "_" :: F query hooks' input slots output m
     | hooks'
     )
     input
@@ -187,3 +360,19 @@ modify_ px f =
       >>= case _ of
           Nothing -> Applicative.pure unit
           Just v -> Hooks.setHookMCons px (f v)
+
+capture ::
+  forall iRL sym' hooks' hooks input slots output m sym v i o.
+  RL.RowToList i iRL =>
+  Sugar.GetLexicalLast "" iRL sym' =>
+  Symbol.Append sym' "_" sym =>
+  IsSymbol sym =>
+  Row.Lacks sym i =>
+  Row.Cons sym v i o =>
+  Row.Lacks sym hooks' =>
+  Row.Cons sym v hooks' hooks =>
+  Eq v =>
+  v ->
+  Hooks.HookM hooks input slots output m Unit ->
+  Hooks.IndexedHookM hooks input slots output m i o Unit
+capture = Sugar.capture
